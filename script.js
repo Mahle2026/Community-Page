ca9a267ea030757244e9524670943d666f413d18
Vandice Tech <amahlephumla80@gmail.com>
2026-06-25 13:42:46 +0200

    commit

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function login() {
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
-    if (username.value.trim() === "" || password.value.trim() === "") {
+    if (username.trim() === "" || password.trim() === "") {
         alert("Please enter your username and password.");
         return;
     }
